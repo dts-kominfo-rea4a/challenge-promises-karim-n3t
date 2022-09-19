231dd6f284e4feab88ea994d31fe5e50c2562661
Fathul Karim <fathul.karim@gmail.com>
2022-09-19 19:14:25 +0700

    promise commit 1

--- promise.js
+++ promise.js
@@ -1,7 +1,29 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async(param) => {
+
+  let data1 = await promiseTheaterIXX();
+  let data2 = await promiseTheaterVGC();
+
+  return new Promise((resolve, rejected) => {
+    let jumlah = 0;
+
+    data1.forEach(element => {
+      if (element.hasil == param) {
+        jumlah++
+      }
+    });
+
+    data2.forEach(element => {
+      if (element.hasil == param) {
+        jumlah++
+      }
+    });
+
+    return resolve(jumlah);
+  });
+};
 
 module.exports = {
   promiseOutput,
